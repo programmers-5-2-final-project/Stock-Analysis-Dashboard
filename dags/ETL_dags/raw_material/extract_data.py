c4a7f90b31358e50c65f820138be87251cd9210d
sanso62 <jmd9762@gmail.com>
2023-08-30 08:25:12 +0000

    [add[ transfrom extracted raw_material data

--- dags/ETL_dags/raw_material/extract_data.py
+++ dags/ETL_dags/raw_material/extract_data.py
@@ -1,4 +1,5 @@
 from ETL_dags.common.extract import Extract
+from ETL_dags.common.transform import Transform
 from ETL_dags.raw_material.constants import FilePath, Ticker
 from ETL_dags.common.csv import df_to_csv
 
@@ -8,6 +9,17 @@ def extract_raw_material_price_data(task_logger, raw_material, start_date, end_d
 
     task_logger.info("Extracting raw_material price")
     raw_df = extract_raw_material.price(start_date, end_date)
+
+    task_logger.info("Transforming extracted data")
+    transform_raw_material = Transform(raw_material, raw_df)
+    if raw_material == "gold":
+        transform_raw_material.drop_nan(["USD (PM)"])
+    if raw_material == "silver":
+        transform_raw_material.drop_nan(["USD"])
+    if raw_material == "cme":
+        transform_raw_material.drop_nan(["Open", "High", "Low", "Last", "Settle"])
+    if raw_material == "orb":
+        transform_raw_material.drop_nan(["Value"])
 
     task_logger.info("Loading extracted data to csv")
     if raw_material == "gold":
